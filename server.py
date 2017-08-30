74d90b9296a9387d01c2632236133924474a74c8
mynameisvinn <vin.tang@gmail.com>
2017-08-30 16:48:16 -0400

    hello

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request, abort
+from keras.models import load_model
+
+app = Flask(__name__)
+model = load_model("models/xor.h5")
+
+@app.route('/api', methods=['GET'])
+def get_tasks():
+    return jsonify({'n_layers': len(model.layers)})
+
+# https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
+@app.route('/api', methods=['PUT'])
+def predict():
+    if not request.json or not 'input' in request.json:
+        abort(400)
+    else:
+        predictions = model.predict(request.json['input']).tolist()
+        return jsonify({"pred_val": predictions})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ train.py
@@ -0,0 +1,26 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD
+import numpy as np 
+
+def create_model(X, y):
+    model = Sequential()
+    model.add(Dense(8, input_dim=2))
+    model.add(Activation('relu'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    sgd = SGD(lr=0.01)
+    model.compile(loss='binary_crossentropy', optimizer=sgd)
+    model.fit(X, y, batch_size=1, epochs=1000, verbose=0)
+    return model
+
+if __name__ == '__main__':
+    X = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
+    y = np.array([[0],[1],[1],[0]], "float32")
+    
+    m = create_model(X, y)
+    m.save("models/xor.h5")
+
+    print "input data: ", X
+    print "predictions: ", m.predict(X)
+    print "predictions: ", m.predict_classes(X)
